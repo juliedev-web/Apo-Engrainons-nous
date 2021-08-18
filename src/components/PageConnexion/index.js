c34d860a00d9e334e1fcc9f560ea0f9922209a79
Yannize <yann.hirlemann@gmail.com>
2021-08-18 11:52:10 +0200

    Footer fixed at bottom 0 in footerDesktop, in connexionPage and signinPage

--- src/components/PageConnexion/index.js
+++ src/components/PageConnexion/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './styles.scss';
+
 // == Composant
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
@@ -8,7 +10,7 @@ import FormConnexion from './FormConnexion';
 const PageConnexion = ({ width, breakpoint }) => {
   console.log('toto');
   return (
-    <div>
+    <div className="page-connexion">
       <Header width={width} breakpoint={breakpoint} />
       <FormConnexion />
       <Footer width={width} breakpoint={breakpoint} />
