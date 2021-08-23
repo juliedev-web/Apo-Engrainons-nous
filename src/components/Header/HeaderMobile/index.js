7a432fefb12250aa635bb46b24112d5b629af3c9
Yannize <yann.hirlemann@gmail.com>
2021-08-23 12:29:12 +0200

    debug NavLink navbarDesktop

--- src/components/Header/HeaderMobile/index.js
+++ src/components/Header/HeaderMobile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import logo from 'src/assets/image/logo.png';
+import logo from 'src/assets/image/Logo.png';
 
 import './styles.scss';
 
