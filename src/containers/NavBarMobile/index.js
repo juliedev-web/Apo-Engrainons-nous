8bd54f53856a1399752228226a4d6b4656f1dfca
Yannize <yann.hirlemann@gmail.com>
2021-08-23 21:50:32 +0200

    menuburger animation, rotation

--- src/containers/NavBarMobile/index.js
+++ src/containers/NavBarMobile/index.js
@@ -4,8 +4,11 @@ import NavBarMobile from 'src/components/Footer/NavBarMobile';
 
 const mapStateToProps = (state) => ({
   isLogged: state.user.isLogged,
+  menuIsOpen: state.user.menuIsOpen,
 });
 
-const mapDispatchToProps = () => ({});
+const mapDispatchToProps = (dispatch) => ({
+  handleMenuIsOpenClick: () => dispatch({ type: 'ON_MENU_CLICK' }),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBarMobile);
